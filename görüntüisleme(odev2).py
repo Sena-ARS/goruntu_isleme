bfd8d58457bd8dcc1f62e198096c75d92b32a2c2
Sena Arslan <95364545+Sena-ARS@users.noreply.github.com>
2023-11-21 00:00:07 +0300

    Update görüntüisleme(odev2).py

--- görüntüisleme(odev2).py
+++ görüntüisleme(odev2).py
@@ -8,10 +8,10 @@ while(True):
     hsv=cv2.cvtColor(video, cv2.COLOR_BGR2HSV)
     dusukKirmizi=np.array([0,12,18])
     yuksekKirmizi=np.array([2,205,252])
-    siyah_beyaz=cv2.inRange(hsv,lower_red,upper_red)
-    renk=cv2.bitwise_and(video,video,mask=sb)
+    siyah_beyaz=cv2.inRange(hsv,yuksekKirmizi,dusukKirmizi)
+    renk=cv2.bitwise_and(video,video,mask=siyah_beyaz)
     cv2=imshow("Renkli",renk)
-    cv2=imshow("Siyah/Beyaz",sb)
+    cv2=imshow("Siyah/Beyaz",siyah_beyaz)
     if cv2.waitkey(50)& 0xFF==ord('x'):
         break
 
